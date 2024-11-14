2e31012d44b7a9dcd2aac2e701dc98d5438f523a
Arvo <cantarvo@gmail.com>
2024-11-14 21:15:58 +0100

    Added optional comments to models
    I've added some comments to the models.py file since I found it to be a little inconspicous. These make the class a little easier to understand.

--- code/models.py
+++ code/models.py
@@ -20,39 +20,69 @@ class NeuralNetworkClassificationModel(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # Convolutional layers
+        #Convolutional layers
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),  # First conv layer
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)  # Downsampling by 2x
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),  #First conv layer, 32 filters to find features --> 2^x for memory allocation
+            nn.ReLU(),  #Activation to add non-linearity
+            nn.MaxPool2d(kernel_size=2, stride=2)  #Pooling layer to reduce size by 2x
         )
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),  # Second conv layer
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),  #Second conv layer with 64 filters
+            nn.ReLU(),  #Another ReLU activation
+            nn.MaxPool2d(kernel_size=2, stride=2)  #Another pooling layer
         )
 
-        # Fully connected layers
-        self.fc1 = nn.Linear(in_features=64 * 7 * 7, out_features=128)
-        self.fc2 = nn.Linear(in_features=128, out_features=64)
-        self.fc3 = nn.Linear(in_features=64, out_features=10)
+        #Fully connected layers
+        self.fc1 = nn.Linear(in_features=64 * 7 * 7, out_features=128)  #first fully connected layer with 128 neurons
+        self.fc2 = nn.Linear(in_features=128, out_features=64)  #second fully connected layer with 64 neurons
+        self.fc3 = nn.Linear(in_features=64, out_features=10)  #output layer for 10 classes
 
     def forward(self, x):
-        # Forward pass through convolutional layers
+        #Forward pass through conv layers
         x = self.layer1(x)
         x = self.layer2(x)
 
-        # Flatten before passing through fully connected layers
+        #Flatten the output to a 1D vector for fully connected layers
         x = x.view(x.size(0), -1)
 
-        # Fully connected layers
+        #Passing through fully connected layers
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
 
+        #Applying softmax to get class probabilities
         return nn.Softmax(dim=1)(x)
 
+"""This huge comment block contains the code for our Third Iteration Neural Network
+    It makes use of Linear Layer functionality and not Conv2D. To test this code you'll have to comment the above NeuralNetworkClassificationModel"""
+# class NeuralNetworkClassificationModel(torch.nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         """ START TODO: fill in all three layers. Remember that each layer should contain 2 parts, a linear transformation and a nonlinear activation function."""
+#         self.layer1 = nn.Sequential(
+#             #nn.Linear(in_features=1, out_features=1),  # Klopt dit? Waar vind ik dit?
+#             nn.Linear(in_features=784, out_features=256), # activation function
+#             nn.ReLU()
+#         )
+#         self.layer2 = nn.Sequential(
+#             nn.Linear(in_features=256, out_features=64),
+#             nn.ReLU() #alpha => Default: 1.0
+#         )
+#
+#         self.layer3 = nn.Sequential(
+#             nn.Linear(in_features=64, out_features=10),  # assuming 3 classes
+#             nn.Softmax(dim=1)  # Softmax along the class dimension -> dimension along which Softmax will be computed(so every slice along dim will sum to 1).
+#         )
+#         """END TODO"""
+#
+#     def forward(self, x: torch.Tensor):
+#         """START TODO: forward tensor x through all layers."""
+#         x = self.layer1(x.flatten(1))
+#         x = self.layer2(x)
+#         x = self.layer3(x)
+#         """END TODO"""
+#         return x
 
 class NeuralNetworkClassificationModelWithVariableLayers(torch.nn.Module):
     def __init__(self, in_size, out_size, hidden_sizes = []):
